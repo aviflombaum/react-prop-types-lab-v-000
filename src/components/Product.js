5fb283c33c8c3ec9f6b678a9d890d9d332ea9966
Avi Flombaum <avi@flombaum.com>
2018-01-12 14:18:50 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,17 @@
 // Code Product Component Here
+import React from 'react';
+
+class Product extends React.Component {
+  render() {
+
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+}
+
+export default Product;
